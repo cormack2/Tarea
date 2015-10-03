11c9a713b77c6ff0a6b25b66401f6a48a7f6a16a
Alex Padilla <a.padilla02@ufromail.cl>
2015-10-02 22:17:22 -0300

    Listo

--- Tarea/src/Calculadora.java
+++ Tarea/src/Calculadora.java
@@ -1,64 +1,110 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Calculadora {
+    
+    public static void menu(Scanner intro){
+        int opc = 0;
+        int num1=0;
+        int num2=0;
+        boolean ciclo=true;
+        do{
+            do{
+            System.out.println("Seleccione la opcion que quiera realizar:");
+            System.out.println("(1)Sumar");
+            System.out.println("(2)Restar");
+            System.out.println("(3)Multiplicar");
+            System.out.println("(4)Division");
+            System.out.println("(5)Salir");	
+            try{
+            opc= intro.nextInt();
+            while(opc>5 || opc<1){
+                System.out.println("Ingrese un numero entre 1 y 5");
+                opc= intro.nextInt();
+            }
+            ciclo = false;
+            }catch(InputMismatchException a){
+                System.out.println("\t error de tipo"+ a);
+                    intro.nextLine();
+                    System.out.println("Sólo se aceptan valores numéricos");
+            }
+            }while(ciclo);
+            ciclo = true;
+            switch (opc){
+                case 1:
+                    suma(intro,num1,num2);
+                    break;
+                case 2:
+                    resta(intro,num1,num2);
+                    break;
+                case 3:
+                    multi(intro,num1,num2);
+                    break;
+                case 4:
+                    division(intro,num1,num2);
+                    break;
+                case 5:
+                    salir(intro,num1);
+                    break;
+                  
+            }
+            
+        }while(opc!=5);
+    }
+    public static void suma(Scanner intro,int num1, int num2){
+        
+        System.out.println("Ingrese numeros");
+        num1=intro.nextInt();
+        num2=intro.nextInt();
+        int suma= num1+num2;
+        System.out.println("El resultado de la suma es:"+suma);
+    }
+    public static void resta(Scanner intro, int num1, int num2){
+        System.out.println("Ingrese numeros");
+        num1=intro.nextInt();
+        num2=intro.nextInt();
+        int resta= num1-num2;
+        System.out.println("El resultado de la resta es:"+resta);
+    }
+    public static void multi(Scanner intro,int num1, int num2){
+        System.out.println("Ingrese numeros");
+        num1=intro.nextInt();
+        num2=intro.nextInt();
+        int multiplicacion=num1*num2;
+        System.out.println("El resultado de la multipliacion"+multiplicacion);
+    }
+    public static void division(Scanner intro,int num1, int num2){
+        System.out.println("Ingrese numeros");
+        num1=intro.nextInt();
+        num2=intro.nextInt();
+        while(num2==0){
+        try {
+        double div=(double)(num1/0) ;
+        }catch(ArithmeticException a){
+            System.out.println("Error, la division por 0 no existe"+a);     
+        }
+            System.out.println("ingrese un numero que no sea cero");
+            num1=intro.nextInt();
+            num2=intro.nextInt();
+        }
+        double div=(double)(num1/num2);
+        System.out.println("El resultado de la division es:"+div);
+    }
+    public static void salir(Scanner intro,int num1){
+        System.out.println("Desea salir");
+        System.out.println("(1)Sí y (2)No");
+        num1=intro.nextInt();
+        if(num1 == 1){
+            System.out.println("Hasta Luego");
+        }
+        if (num1==2){
+            menu(intro);
+        }
+    }
 
-	private static Scanner sc;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	System.out.println("Seleccione la opcion que quiera realizar");
-	System.out.println("(1)Sumar");
-	System.out.println("(2)Restar");
-	System.out.println("(3)Multiplicar");
-	System.out.println("(4)Division");
-	System.out.println("(5)Salir");	
-	sc = new Scanner(System.in);
-	int eleccion = sc.nextInt();
-
-   
-	switch (eleccion) {
-	case 1:
-		 int i = sc.nextInt();
-		    int o = sc.nextInt();
-		    int r;
-		    r = i+o;
-		    System.out.println("El resultado de la suma es:"+r);
-		break;
-	case 2:
-		 int i2 = sc.nextInt();
-		    int o2= sc.nextInt();
-		    int r2;
-		    r2 = i2-o2;
-		    System.out.println("El resultado de la resta es:"+r2);
-		break;
-	case 3:
-		 int i3 = sc.nextInt();
-		    int o3 = sc.nextInt();
-		    int r3;
-		    r3 = i3*o3;
-		    System.out.println("El resultado de la multiplicacion es:"+r3);
-		break;
-	case 4:
-		 int i4 = sc.nextInt();
-		    int o4 = sc.nextInt();
-		    int r4;
-		    r4 = i4/o4;
-		    System.out.println("El resultado de la division es:"+r4);
-		break;
-	case 5:
-		
-		break;
-		
-
-		
-		
-		
-		
-	default:
-		break;
-	}
-	}
+    public static void main(String[] args) {
+       Scanner intro = new Scanner(System.in);
+       menu(intro);
+    }
+    
 }
-
-	
