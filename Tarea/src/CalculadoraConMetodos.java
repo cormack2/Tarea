accaf7e2e5876b51cc0f14efe93a6dafb0d8f9d3
Alex Padilla <a.padilla02@ufromail.cl>
2015-10-17 21:54:25 -0300

    Version Final "Calculadora con Metodos"

--- Tarea/src/CalculadoraConMetodos.java
+++ Tarea/src/CalculadoraConMetodos.java
@@ -1,150 +1,137 @@
-package calculadoraMetodos;
-
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class CalculadoraConMetodos {
 
-	public static int Sumar(int num1, int num2) {
-		return (num1 + num2);
-	}
-
-	public static int Restar(int num1, int num2) {
-		return (num1 - num2);
-	}
-
-	public static int Multiplicar(int num1, int num2) {
-		return (num1 * num2);
-	}
-
-	public static float Dividir(float num1, float num2) {
-		float cociente = (num2 != 0) ? (float) num1 / num2 : num1 / num2;
-		return cociente;
-	}
-
-	public static int Mayor(int num1, int num2) {
-		return Math.max(num1, num2);
-	}
-
-	public static double Potencia(double num1, double num2) {
-		return Math.pow(num1, num2);
-	}
-
-	public static void Menu() {
-		System.out.println("////////////MENU////////////");
-		System.out.println("1.- Sumar dos números");
-		System.out.println("2.- Restar dos números");
-		System.out.println("3.- Multiplicar dos números");
-		System.out.println("4.- Dividir dos números");
-		System.out.println("5.- Mostrar el Mayor de dos numeros");
-		System.out.println("6.- Calcular Potencia");
-		System.out.println("7.- Salir del programa");
-		System.out.println("////////////////////////////");
-		Leeropc();
-	}
-
-	public static void Leeropc() {
-		Scanner scn = new Scanner(System.in);
-		String cadena;
-		int opcion = 0;
-		cadena = scn.nextLine();
-		boolean ciclo1 = true;
+	public static void menu(Scanner intro) {
+		int opc = 0;
+		int num1 = 0;
+		int num2 = 0;
 		boolean ciclo = true;
 		do {
 			do {
-				if (cadena.matches("[0-9]")) {
-					opcion = Integer.parseInt(cadena);
+				System.out.println("Seleccione la opcion que quiera realizar:");
+				System.out.println("(1)Sumar");
+				System.out.println("(2)Restar");
+				System.out.println("(3)Multiplicar");
+				System.out.println("(4)Division");
+				System.out.println("(5)Mayor de los numeros");
+				System.out.println("(6)Potencia, Primero por el Segundo");
+				System.out.println("(7)Salir");
+				try {
+					opc = intro.nextInt();
+					while (opc > 7 || opc < 1) {
+						System.out.println("Ingrese un numero entre 1 y 7");
+						opc = intro.nextInt();
+					}
 					ciclo = false;
-				} else {
-					System.out.println("Opcion Invalida.");
-					System.out.println("----------------");
-					System.out.println("Ingrese una Opcion:");
-					cadena = scn.nextLine();
+				} catch (InputMismatchException a) {
+					System.out.println("\t error de tipo" + a);
+					intro.nextLine();
+					System.out.println("Sólo se aceptan valores numéricos");
 				}
 			} while (ciclo);
-			switch (opcion) {
+			ciclo = true;
+			switch (opc) {
 			case 1:
-				System.out.println("La Suma es: " + Sumar(Leer(scn), Leer(scn)));
+				suma(intro, num1, num2);
 				break;
-
 			case 2:
-				System.out.println("La Resta es: " + Restar(Leer(scn), Leer(scn)));
+				resta(intro, num1, num2);
 				break;
 			case 3:
-				System.out.println("El Producto es: " + Multiplicar(Leer(scn), Leer(scn)));
+				multi(intro, num1, num2);
 				break;
 			case 4:
-				System.out.println("El Cociente es: " + Dividir(Leer(scn), Leer(scn)));
+				division(intro, num1, num2);
 				break;
 			case 5:
-				System.out.println("El Mayor es: " + Mayor(Leer(scn), Leer(scn)));
+				System.out.println("El numero mayor es: " + comparar(intro, num1, num2));
 				break;
 			case 6:
-				System.out.println("La Potencia es: " + Potencia(Leer(scn), Leer(scn)));
+				System.out.println("El resultado de la potencia es: " + potencia(intro, num1, num2));
 				break;
 			case 7:
-				Salir(scn);
-				break;
-
-			default:
-				System.out.println("-----------------------------------");
-				System.out.println("Opción inválida; ingrese de nuevo.");
+				salir(intro, num1);
 				break;
 
 			}
-			while (opcion != 7)
-				Menu();
-			ciclo1 = false;
 
-		} while (ciclo1);
-
+		} while (opc != 7);
 	}
 
-	public static int Leer(Scanner scn) {
-		int numero = 0;
-		boolean ciclo = true;
-		System.out.println("Ingrese un Numero:");
-		String cadena;
-		cadena = scn.nextLine();
-		do {
-			if (cadena.matches("[0-9]")) {
-				numero = Integer.parseInt(cadena);
-				ciclo = false;
-			} else {
-				System.out.println("No es un Número.");
-				System.out.println("----------------");
-				System.out.println("Ingrese un Número:");
-				cadena = scn.nextLine();
-			}
-		} while (ciclo);
-		return numero;
+	public static void suma(Scanner intro, int num1, int num2) {
+		System.out.println("Ingrese numeros");
+		num1 = intro.nextInt();
+		num2 = intro.nextInt();
+		int suma = num1 + num2;
+		System.out.println("El resultado de la suma es: " + suma);
 	}
 
-	public static void Salir(Scanner scn) {
-		int num1 = 0;
-		boolean ciclo = true;
-		do {
-			System.out.println("¿De verdad quiere salir?(1.SI/2.NO)");
-			num1 = Leer(scn);
-			while (num1 > 2 || num1 < 1) {
-				System.out.println("Opción inválida, ingrese de nuevo");
-				Leer(scn);
+	public static void resta(Scanner intro, int num1, int num2) {
+		System.out.println("Ingrese numeros");
+		num1 = intro.nextInt();
+		num2 = intro.nextInt();
+		int resta = num1 - num2;
+		System.out.println("El resultado de la resta es: " + resta);
+	}
+
+	public static void multi(Scanner intro, int num1, int num2) {
+		System.out.println("Ingrese numeros");
+		num1 = intro.nextInt();
+		num2 = intro.nextInt();
+		int multiplicacion = num1 * num2;
+		System.out.println("El resultado de la multipliacion es: "
+				+ multiplicacion);
+	}
+
+	public static void division(Scanner intro, int num1, int num2) {
+		System.out.println("Ingrese numeros");
+		num1 = intro.nextInt();
+		num2 = intro.nextInt();
+		while (num2 == 0) {
+			try {
+				double div = (double) (num1 / 0);
+			} catch (ArithmeticException a) {
+				System.out.println("Error, la division por 0 no existe" + a);
 			}
-			if (num1 == 1) {
-				System.out.println("Hasta Luego...");
-			}
-			if (num1 == 2) {
-				System.out.println("-----------------------------");
-				Menu();
-			}
-			ciclo = false;
-		} while (ciclo);
-	};
+			System.out.println("ingrese un numero que no sea cero");
+			num1 = intro.nextInt();
+			num2 = intro.nextInt();
+		}
+		double div = (double) (num1 / num2);
+		System.out.println("El resultado de la division es: " + div);
+	}
+
+	public static int comparar(Scanner intro, int num1, int num2) {
+		System.out.println("Ingrese numeros");
+		num1 = intro.nextInt();
+		num2 = intro.nextInt();
+		return Math.max(num1, num2);
+	}
+
+	public static int potencia(Scanner intro, int num1, int num2) {
+		System.out.println("Ingrese numeros");
+		num1 = intro.nextInt();
+		num2 = intro.nextInt();
+		return (int) Math.pow(num1, num2);
+	}
+
+	public static void salir(Scanner intro, int num1) {
+		System.out.println("Desea salir");
+		System.out.println("(1)Sí y (2)No");
+		num1 = intro.nextInt();
+		if (num1 == 1) {
+			System.out.println("Hasta Luego");
+		}
+		if (num1 == 2) {
+			menu(intro);
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Menu();
-
+		Scanner intro = new Scanner(System.in);
+		menu(intro);
 	}
 
 }
